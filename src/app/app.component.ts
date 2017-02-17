ce400b432d99469675f45d8e20ad202066837387
tomekbielaszewski <tomekbielaszewski@gmail.com>
2017-02-17 16:01:27 +0100

    gitignoring js and map files as app is written in TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero}} details!</h2>
+`
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
