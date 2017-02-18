733c8dd11761754241b78a833ee96734b0afbc85
Grizwold <tomekbielaszewski@gmail.com>
2017-02-18 01:18:11 +0100

    angular router full setup with router module extracted to separate file. Router links with parameters in HTML. Routing from TS. Styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import {Component} from '@angular/core'
 
 @Component({
+  moduleId: module.id,
   selector: 'app',
   template: `
     <h1>{{title}}</h1>
     
-    <a routerLink="/dashboard">Dashboard</a>
-    <a routerLink="/heroes">Heroes</a>
+    <nav>
+      <a routerLinkActive="active" routerLink="/dashboard">Dashboard</a>
+      <a routerLinkActive="active" routerLink="/heroes">Heroes</a>
+    </nav>
     
     <router-outlet></router-outlet>
-`
+`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public title = 'Tour of Heroes';
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,21 +1,35 @@
-import {Component, Input} from '@angular/core';
-import {Hero} from './hero';
+import 'rxjs/add/operator/switchMap';
+
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Location} from '@angular/common';
+
+import {HeroService} from './hero.service';
+import {Hero} from "./hero";
 
 @Component({
+  moduleId: module.id,
   selector: 'hero-detail',
-  template: `
-    <div *ngIf='hero'>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]='hero.name' placeholder='name'/>
-      </div>
-    </div>
-`
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 
-export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  public hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {};
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((param: Params) => this.heroService.getHero(+param['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  public goBack(): void {
+    this.location.back();
+  }
 }
